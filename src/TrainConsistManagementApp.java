3c70849154e6d8ab2c91f2d9f5bbb54241269c86
sumanth-batna29 <batnasumanth2007@gmail.com>
2026-03-25 14:30:05 +0530

    UC1: Display Consist Summary

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,6 +1,24 @@
+public class TrainConsistManagementApp {
+    static class Train {
+        String trainId;
+        String name;
+        int totalBogies;
 
-    public class TrainConsistManagementApp {
-        public static void main(String[] args) {
-            System.out.println("Train Consist Management App - Initialized");
+        public Train(String trainId, String name, int totalBogies) {
+            this.trainId = trainId;
+            this.name = name;
+            this.totalBogies = totalBogies;
+        }
+
+        public void displayConsistSummary() {
+            System.out.println("Train ID: " + trainId);
+            System.out.println("Train Name: " + name);
+            System.out.println("Total Bogies: " + totalBogies);
         }
     }
+
+    public static void main(String[] args) {
+        Train train = new Train("TR001", "Express", 10);
+        train.displayConsistSummary();
+    }
+}
